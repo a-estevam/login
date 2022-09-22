e43d111e6cf5414ef35c1ba2788f8a735a083a04
a-estevam <113124669+a-estevam@users.noreply.github.com>
2022-09-22 15:31:40 -0300

    JS funcionando

---
+++ script.js
@@ -0,0 +1,13 @@
+function logar(){
+    const usuario = document.getElementById('txtusuario').value;
+    const senha = document.getElementById('txtsenha').value;
+    
+    if(usuario == "admin" && senha =="admin"){
+        alert("SUCESSO");
+    } else{
+        alert("USUÁRIO ou SENHA INVÁLIDOS")
+    }
+console.log(usuario)
+console.log(senha)
+}
+
