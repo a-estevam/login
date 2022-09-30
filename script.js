213a1752aa52b49c4bfa21873433d69eec207d6d
a-estevam <estevam.adesign@gmail.com>
2022-09-30 11:59:07 -0300

    teste

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+// import app from "./firebase/app.js";
+//  console.log(app)
+
 function logar(){
     const usuario = document.getElementById('txtusuario').value;
     const senha = document.getElementById('txtsenha').value;
-    
+    const btnlogin = document.getElementById('btn-login')
+
     if(usuario == "admin" && senha =="admin"){
         alert("SUCESSO");
     } else{
         alert("USUÁRIO ou SENHA INVÁLIDOS")
     }
+
+    
+    // btnlogin.addEventListener('click',() => {
+    //     // const subscription ={
+    //     //     Login: usuario.value,
+    //     //     password: senha.value,
+    //     // }
+    // })
+
 console.log(usuario)
 console.log(senha)
+console.log(btnlogin)
 }
 
