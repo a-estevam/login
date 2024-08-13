27c44cfdf9894f31a161552c5f77efda394c29ed
Antonio Estevam <113124669+a-estevam@users.noreply.github.com>
2024-08-13 16:56:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-
-function novoLogar(){
+function novoLogar() {
     const usuario = document.getElementById('txtusuario').value;
     const senha = document.getElementById('txtsenha').value;
-    const btnlogin = document.getElementById('btn-login')
 
-    if(usuario =="admin" && senha =="admin"){
+    if (usuario === "admin" && senha === "admin") {
         alert("SUCESSO");
-    } else{
-        alert("USUÁRIO ou SENHA INVÁLIDOS")
+    } else {
+        alert("USUÁRIO ou SENHA INVÁLIDOS");
     }
 
-    
-    // btnlogin.addEventListener('click',() => {
-    //     // const subscription ={
-    //     //     Login: usuario.value,
-    //     //     password: senha.value,
-    //     // }
-    // })
-
-console.log(usuario)
-console.log(senha)
-console.log(btnlogin)
+    console.log(usuario);
+    console.log(senha);
 }
-
