829ff92385cae4b972b2838866a32174b33bfa91
Antonio Estevam <113124669+a-estevam@users.noreply.github.com>
2024-08-13 16:45:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// import app from "./firebase/app.js";
-//  console.log(app)
 
 function logar(){
     const usuario = document.getElementById('txtusuario').value;
     const senha = document.getElementById('txtsenha').value;
     const btnlogin = document.getElementById('btn-login')
 
-    if(usuario == "admin" && senha =="admin"){
+    if(usuario =="admin" && senha =="admin"){
         alert("SUCESSO");
     } else{
         alert("USUÁRIO ou SENHA INVÁLIDOS")
